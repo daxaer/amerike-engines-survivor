698df0087f47775f0bb280002679d2a6caccaf86
daxaer <48969078+daxaer@users.noreply.github.com>
2024-03-05 20:51:12 -0600

    using inecesarios

--- Assets/Script/Mapas/views/MapView.cs
+++ Assets/Script/Mapas/views/MapView.cs
@@ -1,12 +1,6 @@
-using Map.controllers;
 using Map.views;
-using Player.views;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using Utils.Bindings;
+
 
 public class MapView : MonoBehaviour, IMapView
 {
